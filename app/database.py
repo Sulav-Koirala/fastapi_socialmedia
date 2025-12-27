bb8f13b0a9c398d01703e9efaa5df84d8e2d1723
Sulav-Koirala <koiralasulav0@gmail.com>
2025-12-27 13:09:59 +0545

    commenting out

--- app/database.py
+++ app/database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from .config import settings
-import os
+#import os
 
-sqlalchemy_db_url=os.getenv("DATABASE_URL")
-if not sqlalchemy_db_url:
-    sqlalchemy_db_url=f"postgresql://{settings.db_username}:{settings.db_pwd}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}"
+#sqlalchemy_db_url=os.getenv("DATABASE_URL")
+#if not sqlalchemy_db_url:
+sqlalchemy_db_url=f"postgresql://{settings.db_username}:{settings.db_pwd}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}"
 engine=create_engine(sqlalchemy_db_url)
 session_local=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
