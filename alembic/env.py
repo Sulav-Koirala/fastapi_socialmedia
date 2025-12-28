1e73f86f1577f8e2a0e5a41b695db3579988c88c
Sulav-Koirala <koiralasulav0@gmail.com>
2025-12-28 20:36:31 +0545

    Normalize line endings to LF

--- alembic/env.py
+++ alembic/env.py
@@ -5,17 +5,23 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.models import Base
-from app.config import settings
+# from app.config import settings
+import os
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
 config = context.config
-config.set_main_option("sqlalchemy.url",f"postgresql://{settings.db_username}:{settings.db_pwd}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}")
+# config.set_main_option("sqlalchemy.url",f"postgresql://{settings.db_username}:{settings.db_pwd}@{settings.db_hostname}:{settings.db_port}/{settings.db_name}")
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
